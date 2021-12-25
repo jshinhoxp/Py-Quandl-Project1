0c66ab98c52301bc96d0d3cf0b1fc8701400cab2
Shin Ho Jung <jshinhoxp@gmail.com>
2021-12-24 16:00:50 -1000

    Added MatLab Graph

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import yfinance as yf
 
 start_date = '1990-01-01'
-end_date = '2021-07-12'
+end_date = '2022-01-01'
 
 tickers_list = ['AAPL', 'IBM', 'MSFT', 'WMT']
 
@@ -16,3 +16,14 @@ for ticker in tickers_list:
 
 data.head()
 print(data)
+
+data.plot(figsize=(10, 7))
+
+plt.legend()
+plt.title("Adjusted Close Price", fontsize=16)
+
+plt.ylabel('Price', fontsize=14)
+plt.xlabel('Year', fontsize=14)
+
+plt.grid(which="major", color='k', linestyle='-.', linewidth=0.5)
+plt.show()
